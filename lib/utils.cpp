e72ad93c2d45cc27686dd14cb5975f8d1485bf41
Christopher Fujino <christopherfujino@gmail.com>
2026-01-30 17:11:14 -0800

    use namespace

--- lib/utils.cpp
+++ lib/utils.cpp
@@ -6,12 +6,26 @@ export module Utils;
 
 namespace fs = std::filesystem;
 
+namespace __chris_monorepo__cpp__cleaner {
+
+fs::path _normalize(const char *raw) {
+  fs::path script = fs::canonical(fs::path(raw));
+
+#ifndef __cpp_guaranteed_copy_elision
+  #error "no __cpp_guaranteed_copy_elision present"
+#endif
+  // TODO __cpp_implicit_move once 2023
+
+  return script.parent_path();
+}
+
 export void init(int argc, char **argv) {
-  fs::path script = fs::canonical(fs::path(argv[0]));
-  fs::path scriptDir = script.parent_path();
+  auto scriptDir = _normalize(argv[0]);
   printf("scriptDir = %s\n", scriptDir.c_str());
 
   for (int i = 1; i < argc; i++) {
     printf("[%d] %s\n", i, argv[i]);
   }
 }
+
+} // namespace __chris_monorepo__cpp__cleaner
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 import Utils;
 
+using namespace __chris_monorepo__cpp__cleaner;
+
 int main(int argc, char **argv) {
   init(argc, argv);
   printf("end.\n");
